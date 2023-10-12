1071c6fa7bb74acfa6cbb6dbd2e2b5337d3f20a4
David Martínez Trigo <davidmtrigo@hotmail.es>
2023-10-12 21:03:23 +0200

    feat(scaffolding): deleted cardComponent at routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
 
-import { CardComponent } from './shared/components/card/card.component';
-
 const routes: Routes = [
-  {
-    path: '',
-    component: CardComponent,
-  },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
